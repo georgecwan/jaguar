96c06e0f9597d1341b47165b392afd6c72ef4e51
Rick Zhang <rz6zhang@uwaterloo.ca>
2021-11-04 02:46:07 -0400

    added left-right and right-left turns in motor test

--- BaseLibrary/Code/Server/test.py
+++ BaseLibrary/Code/Server/test.py
@@ -33,32 +33,40 @@ PWM = Motor()
 
 def test_Motor():
     try:
-        PWM.setMotorModel(1000, 1000, 1000, 1000)  # Forward
-        print("The car is moving forward")
-        time.sleep(1)
 
-        PWM.setMotorModel(-1000, -1000, -1000, -1000)  # Back
-        print("The car is going backwards")
-        time.sleep(1)
-        PWM.setMotorModel(1000, 1000, 1000, 1000)  # Forward
-        print("The car is moving forward")
-        time.sleep(1)
-
-        PWM.setMotorModel(-1000, -1000, -1000, -1000)  # Back
-        print("The car is going backwards")
-        time.sleep(1)
-        PWM.setMotorModel(0, 0, 0, 0)  # Stop
-        time.sleep(5)
-        PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
-
-        print("The car is turning right")
-        time.sleep(1)
-        PWM.setMotorModel(0, 0, 0, 0)  # Stop
-        time.sleep(5)
         PWM.setMotorModel(-1500, -1500, 2000, 2000)  # Left
-
         print("The car is turning left")
         time.sleep(1)
+
+        PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
+        print("The car is turning right")
+        time.sleep(1)
+
+        PWM.setMotorModel(0, 0, 0, 0)  # Stop
+        time.sleep(2)
+
+        PWM.setMotorModel(1000, 1000, 1000, 1000)  # Forward
+        print("The car is moving forward")
+        time.sleep(1)
+
+        PWM.setMotorModel(-1000, -1000, -1000, -1000)  # Back
+        print("The car is going backwards")
+        time.sleep(1)
+
+        PWM.setMotorModel(0, 0, 0, 0)  # Stop
+        time.sleep(3)
+
+        PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
+        print("The car is turning right")
+        time.sleep(1)
+
+        PWM.setMotorModel(0, 0, 0, 0)  # Stop
+        time.sleep(3)
+
+        PWM.setMotorModel(-1500, -1500, 2000, 2000)  # Left
+        print("The car is turning left")
+        time.sleep(1)
+
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         print("\nEnd of program")
     except KeyboardInterrupt:
