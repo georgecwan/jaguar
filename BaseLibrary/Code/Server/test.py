843b8c27e08879ad4c160f9a42fae029591a1263
Rick Zhang <rz6zhang@uwaterloo.ca>
2021-11-04 03:44:23 -0400

    reduce left wheel, add stop between front and back

--- BaseLibrary/Code/Server/test.py
+++ BaseLibrary/Code/Server/test.py
@@ -41,7 +41,7 @@ def test_Motor():
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         time.sleep(5)
 
-        PWM.setMotorModel(-2200, -1500, 2000, 2000)  # Left
+        PWM.setMotorModel(-2100, -1500, 2000, 2000)  # Left
         print("The car is turning left")
         time.sleep(1)
 
@@ -52,6 +52,9 @@ def test_Motor():
         print("The car is moving forward")
         time.sleep(1)
 
+        PWM.setMotorModel(0, 0, 0, 0)  # Stop
+        time.sleep(1)
+
         PWM.setMotorModel(-1000, -1000, -1000, -1000)  # Back
         print("The car is going backwards")
         time.sleep(1)
@@ -59,7 +62,7 @@ def test_Motor():
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         time.sleep(5)
 
-        PWM.setMotorModel(-2200, -1500, 2000, 2000)  # Left
+        PWM.setMotorModel(-2100, -1500, 2000, 2000)  # Left
         print("The car is turning left")
         time.sleep(1)
 
