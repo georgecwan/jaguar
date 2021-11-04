76023eb07572929b6921e450fb899cea5e4e3686
Rick Zhang <rz6zhang@uwaterloo.ca>
2021-11-04 02:14:55 -0400

    reverse left-right order of test motor

--- BaseLibrary/Code/Server/test.py
+++ BaseLibrary/Code/Server/test.py
@@ -44,15 +44,15 @@ def test_Motor():
         time.sleep(1)
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         time.sleep(1)
+        PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
 
-        PWM.setMotorModel(-1500, -1500, 2000, 2000)  # Left
-        print("The car is turning left")
+        print("The car is turning right")
         time.sleep(1)
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         time.sleep(1)
+        PWM.setMotorModel(-1500, -1500, 2000, 2000)  # Left
 
-        PWM.setMotorModel(2000, 2000, -1500, -1500)  # Right
-        print("The car is turning right")
+        print("The car is turning left")
         time.sleep(1)
         PWM.setMotorModel(0, 0, 0, 0)  # Stop
         print("\nEnd of program")
