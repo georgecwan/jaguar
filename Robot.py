d1292219f92cbd018956b0f362546b947430b8da
GcWan <GcWan@users.noreply.github.com>
2021-11-07 16:51:05 -0500

    Add movement code based on faceDetect

--- Robot.py
+++ Robot.py
@@ -1,11 +1,24 @@
-# from BaseLibrary.Code.Server.Motor import *
+from BaseLibrary.Code.Server.Motor import*
 from cv.faceDetect import Vision
 import time
 
 cv = Vision()
+PWM = Motor()
 
 # Main robot loop goes here
 while True:
     (x, y, w, h) = cv.get_bounding_box();
     print(f"{x}, {y}, {w}, {h}")
+    if (w>0 and h>0):
+        if (w>120 and h>120):
+            # Moves backward
+            PWM.setMotorModel(-1000, -1000, -1000, -1000)
+        elif(w<80 and h<80):
+            # Moves forward
+            PWM.setMotorModel(1000, 1000, 1000, 1000)
+        else:
+            # Stop
+            PWM.setMotorModel(0, 0, 0, 0)
+
+
     
