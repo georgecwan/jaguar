b529e5af4a4542e1f8a055adaa129f650876fb74
GcWan <GcWan@users.noreply.github.com>
2021-11-07 17:42:26 -0500

    Adjust turning values

--- Robot.py
+++ Robot.py
@@ -16,19 +16,19 @@ try:
         if abs(relativeX) >= 100:
             if relativeX < 0:
                 print("Turning right")
-                m1, m2, m3, m4 = m1 + 200, m2 + 200, m3 - 200, m4 - 200
+                m1, m2, m3, m4 = m1 + 500, m2 + 500, m3 - 500, m4 - 500
             elif relativeX > 0:
                 print("Turning left")
-                m1, m2, m3, m4 = m1 - 200, m2 - 200, m3 + 200, m4 + 200
+                m1, m2, m3, m4 = m1 - 500, m2 - 500, m3 + 500, m4 + 500
 
         if w > 120 and h > 120:
             # Too close
             print("Going backwards")
-            m1, m2, m3, m4 = m1 - 800, m2 - 800, m3 - 800, m4 - 800
+            m1, m2, m3, m4 = m1 - 1000, m2 - 1000, m3 - 1000, m4 - 1000
         elif 0 < w < 80 and 0 < h < 80:
             # Too far
             print("Going forwards")
-            m1, m2, m3, m4 = m1 + 800, m2 + 800, m3 + 800, m4 + 800
+            m1, m2, m3, m4 = m1 + 1000, m2 + 1000, m3 + 1000, m4 + 1000
         else:
             # Stop
             print("No forwards/backwards movement")
