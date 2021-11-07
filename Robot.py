cafb77992f1421f74c414ad19e7b2f877b4cf1a7
Jason Yuan <j227yuan@uwaterloo.ca>
2021-11-07 16:58:30 -0500

    Add destruct commands on KeyboardInterrupt.

--- Robot.py
+++ Robot.py
@@ -5,20 +5,28 @@ import time
 cv = Vision()
 PWM = Motor()
 
-# Main robot loop goes here
-while True:
-    (x, y, w, h) = cv.get_bounding_box();
-    print(f"{x}, {y}, {w}, {h}")
-    if (w>0 and h>0):
-        if (w>120 and h>120):
-            # Moves backward
-            PWM.setMotorModel(-1000, -1000, -1000, -1000)
-        elif(w<80 and h<80):
-            # Moves forward
-            PWM.setMotorModel(1000, 1000, 1000, 1000)
-        else:
-            # Stop
-            PWM.setMotorModel(0, 0, 0, 0)
+
+try:
+    # Main robot loop goes here
+    while True:
+        (x, y, w, h) = cv.get_bounding_box()
+        # print(f"{x}, {y}, {w}, {h}")
+        if w > 0 and h > 0:
+            if w > 120 and h > 120:
+                # Moves backward
+                print("Going backwards")
+                PWM.setMotorModel(-1000, -1000, -1000, -1000)
+            elif w < 80 and h < 80:
+                # Moves forward
+                print("Going forwards")
+                PWM.setMotorModel(1000, 1000, 1000, 1000)
+            else:
+                # Stop
+                print("Staying still")
+                PWM.setMotorModel(0, 0, 0, 0)
+except KeyboardInterrupt:
+    PWM.destroy()
+    cv.destroy()
 
 
     
