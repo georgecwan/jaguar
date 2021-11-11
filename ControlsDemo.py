014455e7d5670492d22c4497e55cc69335883e32
Jason Yuan <j227yuan@uwaterloo.ca>
2021-11-11 15:41:48 -0500

    Add manual controls to controls demo

--- ControlsDemo.py
+++ ControlsDemo.py
@@ -5,25 +5,18 @@ PWM = Motor()
 
 
 try:
-    PWM.setMotorModel(1000, 1000, 1000, 1000)
-    print("The car is going forwards")
-    input()
-    PWM.setMotorModel(0, 0, 0, 0)
-    input()
-    PWM.setMotorModel(-1000, -1000, -1000, -1000)
-    print("The car is going backwards")
-    input()
-    PWM.setMotorModel(0, 0, 0, 0)
-    input()
-    PWM.setMotorModel(-1900, -1500, 2000, 2000)  # Left
-    print("The car is turning left")
-    input()
-    PWM.setMotorModel(0, 0, 0, 0)
-    input()
-    PWM.setMotorModel(2000, 2000, -2500, -1500)  # Right
-    print("The car is turning right")
-    input()
-    PWM.setMotorModel(0, 0, 0, 0)
+    while True:
+        a = input()
+        if a == 'w':
+            PWM.setMotorModel(1000, 1000, 1000, 1000)
+        elif a == 'a':
+            PWM.setMotorModel(-1900, -1500, 2000, 2000)
+        elif a == 's':
+            PWM.setMotorModel(-1000, -1000, -1000, -1000)
+        elif a == 'd':
+            PWM.setMotorModel(2000, 2000, -2500, -1500)
+        else:
+            PWM.setMotorModel(0, 0, 0, 0)
 
 except KeyboardInterrupt:
     PWM.setMotorModel(0, 0, 0, 0)
