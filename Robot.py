497b211a032d650d2bb60576cab28bae6832bb46
GcWan <GcWan@users.noreply.github.com>
2021-11-11 13:52:03 -0500

    Fix merge errors

--- Robot.py
+++ Robot.py
@@ -28,26 +28,26 @@ try:
         if w > 100 and h > 100:
             # Too close
             print("Going backwards")
-            m1, m2, m3, m4 = m1 - 800, m2 - 800, m3 - 800, m4 - 800
+            m1i, m2i, m3i, m4i = -800, -800, -800, -800
             led.colorWipe(led.strip, Color(255, 0, 0))
             idleCount = 0
         elif 0 < w < 60 and 0 < h < 60:
             # Too far
             print("Going forwards")
-            m1, m2, m3, m4 = m1 + 800, m2 + 800, m3 + 800, m4 + 800
+            m1i, m2i, m3i, m4i = 800, 800, 800, 800
             led.colorWipe(led.strip, Color(0, 255, 0))
             idleCount = 0
         elif idleCount < 5:
             print("Idling")
             idleCount += 1
-            led.colorWipe(led.strip, Color(0, 0, 255))
         else:
             print("No f/b movement")
             m1i, m2i, m3i, m4i = 0, 0, 0, 0
+            led.colorWipe(led.strip, Color(0, 0, 255))
 
         PWM.setMotorModel(m1t + m1i, m2t + m2i, m3t + m3i, m4t + m4i)
 
 except KeyboardInterrupt:
     PWM.setMotorModel(0, 0, 0, 0)
     cv.destroy()
-    led.colorWipe(led.strip, Color(0,0,0),10)
+    led.colorWipe(led.strip, Color(0, 0, 0), 10)
