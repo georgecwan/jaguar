1181f7e15ef61ff53eed22cdd805fe9799124e47
Rick Zhang <rz6zhang@uwaterloo.ca>
2021-11-21 15:38:02 -0500

    test forward and turn control

--- ControlsDemo.py
+++ ControlsDemo.py
@@ -8,16 +8,23 @@ try:
     while True:
         a = input()
         if a == 'w':
-            PWM.setMotorModel(1000, 1000, 1000, 1000)
+            PWM.setMotorModel(1100, 1100, 1000, 1000)
         elif a == 'a':
             PWM.setMotorModel(-1900, -1500, 2000, 2000)
         elif a == 's':
             PWM.setMotorModel(-1000, -1000, -1000, -1000)
         elif a == 'd':
             PWM.setMotorModel(2000, 2000, -2500, -1500)
+        elif a == 'q':
+            PWM.setMotorModel(600, 600, 1300, 1300)
+        elif a == 'e':
+            PWM.setMotorModel(1300, 1300, 600, 600)
+        elif a == 'j':
+            PWM.setMotorModel(600, 600, 1600, 1600)
+        elif a == 'l':
+            PWM.setMotorModel(1300, 1300, 600, 600)
         else:
             PWM.setMotorModel(0, 0, 0, 0)
 
 except KeyboardInterrupt:
     PWM.setMotorModel(0, 0, 0, 0)
-
