8b2e17c042f8828116dbf28e3fc33aa7b8f04759
Jason Yuan <j227yuan@uwaterloo.ca>
2021-11-28 14:52:03 -0500

    Implement always listening voice script.

--- Voice.py
+++ Voice.py
@@ -7,31 +7,22 @@ from multiprocessing.shared_memory import SharedMemory
 class Voice:
     def __init__(self, shared_memory_name):
         self.shared_mode = SharedMemory(name=shared_memory_name, create=False)
-        self.state = False
         self.script_helper = None
-        self.wake_wait = None
 
     def start(self):
         # Start the wake word detection and the speech recognition.
-        self.state = True
         self.run()
 
     def run(self):
-        # Uses the voice2json software to listen for the wake word
-        while self.state:
-            # These two commands are blocking
-            # The first command waits for a wake word then exits
-            # The second command listens for the actual command, parses it, and exits
-            self.wait_wait = subprocess.Popen(["/usr/bin/voice2json", "-p", "/home/pi/.local/share/voice2json/en-us_kaldi-zamia", "wait-wake", "--exit-count", "1"]).wait()
-            
-            # Tell the car to stop while the system is running
-            self.shared_mode.buf[0] = 0
             self.script_helper = subprocess.Popen(["./voice_helper.sh"], stdout=subprocess.PIPE, universal_newlines=True)
 
             # Read the output of the speech recognition
             for stdout_line in iter(self.script_helper.stdout.readline, ""):
                 if "Command: " in stdout_line:
-                    func_name = stdout_line.split()[1]
+                    try:
+                        func_name = stdout_line.split()[1]
+                    except IndexError:
+                        continue
                     if func_name == "SitDown":
                         self.shared_mode.buf[0] = 0
                     elif func_name == "Follow":
@@ -43,11 +34,8 @@ class Voice:
             self.script_helper.stdout.close()
 
     def stop(self):
-        self.state = False
         if self.script_helper is not None:
             self.script_helper.terminate()
-        if self.wake_wait is not None:
-            self.wake_wait.terminate()
         time.sleep(3)
         self.shared_mode.close()
 
@@ -56,4 +44,7 @@ if __name__ == "__main__":
     script_helper = subprocess.Popen(["./voice_helper.sh"], stdout=subprocess.PIPE, universal_newlines=True)
     for stdout_line in iter(script_helper.stdout.readline, ""):
         if "Command: " in stdout_line:
-            print(stdout_line.split()[1])
+            try:
+                print(stdout_line.split()[1])
+            except IndexError:
+                continue
